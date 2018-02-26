b1e4ddb826941d680a5b50a6d05aa6525d731037
Prathyusha Mattam <prathyusha.mattam@gmail.com>
2018-02-25 23:23:18 -0500

    Added functionality for arrow buttons and close button

--- main.js
+++ main.js
@@ -1,52 +1,124 @@
 var container = document.querySelector('.container');
-
-var lightBox = function () {
-    lightbox = document.querySelector(".lightbox");
-    lightBoxImg = document.querySelector(".lightbox  img");
-    lightBoxImg.setAttribute("src", this.getAttribute("src"));
-    lightbox.classList.add("active");
-    close = document.querySelector(".close");
-    close.classList.add("active");
-};
-
-var closeImg = function () {
-    console.log("You clicked me");
-    lightbox = document.querySelector(".lightbox");
-    lightbox.classList.remove("active");
-    close = document.querySelector(".close");
-    close.classList.remove("active");    
-};
-
-document.querySelector(".close");
-document.querySelector(".close-img").addEventListener("click", closeImg);
-
-var img_urls = [
-    "images/img1.jpeg",
-    "images/img2.jpeg",
-    "images/img3.jpeg",
-    "images/img4.jpeg",
-    "images/img5.jpeg",
-    "images/img6.jpeg",
-    "images/img7.jpeg",
-    "images/img8.jpeg",
-    "images/img9.jpeg",
-    "images/img10.jpeg",
-    "images/img11.jpeg",
-    "images/img12.jpeg",
-    "images/img13.jpeg",
-    "images/img14.jpeg",
-    "images/img15.jpeg"
+var lightBox = document.querySelector(".lightbox");
+var lightBoxImg = document.querySelector(".lb-img  img");
+var close = document.querySelector(".close");
+var larrow = document.querySelector(".larrow");
+var rarrow = document.querySelector(".rarrow");
+var imgArray = [
+    {
+        link: "images/img1.jpeg"
+    },
+    {
+        link: "images/img2.jpeg"
+    },
+    {
+        link: "images/img3.jpeg"
+    },
+    {
+        link: "images/img4.jpeg"
+    },
+    {
+        link: "images/img5.jpeg"
+    },
+    {
+        link: "images/img6.jpeg"
+    },
+    {
+        link: "images/img7.jpeg"
+    },
+    {
+        link: "images/img8.jpeg"
+    },
+    {
+        link: "images/img9.jpeg"
+    },
+    {
+        link: "images/img10.jpeg"
+    },
+    {
+        link: "images/img11.jpeg"
+    },
+    {
+        link: "images/img12.jpeg"
+    },
+    {
+        link: "images/img13.jpeg"
+    },
+    {
+        link: "images/img14.jpeg"
+    },
+    {
+        link: "images/img15.jpeg"
+    }    
 ];
 
-var displayImages = function (img_urls) { 
-    for(i=0; i<img_urls.length; i++) {
-        var image = document.createElement('img');
-        image.setAttribute('src', img_urls[i]);
-        image.classList.add('img_'+(i+1));
-        container.appendChild(image);   
-        image.addEventListener('click', lightBox);
+// forEach with a function expression!
+imgArray.forEach(function(image) {
+    var imageTag = document.createElement('img');
+    imageTag.src = image.link;
+    container.appendChild(imageTag);
+    // imageTag.classList.add('img_class');
+    imageTag.addEventListener('click', function() {
+        lightBoxImg.src = image.link;
+        selectedImg = image.link; 
+        lightBox.classList.add("active");
+        close.classList.add("active");
+        var currentImgNumber = selectedImg.substring(selectedImg.lastIndexOf("img")+3, selectedImg.lastIndexOf(".jpeg"));
+        if(parseInt(currentImgNumber) > 1) {
+            larrow.classList.add("active");
+        }
+        if(parseInt(currentImgNumber) < 15) {
+            rarrow.classList.add("active");
+        }    
+    })
+});
+
+var leftArrowFn = function() {
+    var currentImgNumber = selectedImg.substring(selectedImg.lastIndexOf("img")+3, selectedImg.lastIndexOf(".jpeg"));
+    if(parseInt(currentImgNumber) > 1) {
+        var prevImgSrc = "images/img" + (parseInt(currentImgNumber)-1) + ".jpeg"; 
+        lightBoxImg.setAttribute("src", prevImgSrc);
+        selectedImg = prevImgSrc;
+        if(parseInt(currentImgNumber) === 2){
+            larrow.classList.remove("active");
+        }
+        if(parseInt(currentImgNumber) === 15){
+            rarrow.classList.add("active");
+        }
+    }
+    else {
+        larrow.classList.remove("active");
     }
 };
 
-displayImages(img_urls);
+var rightArrowFn = function() {
+    var currentImgNumber = selectedImg.substring(selectedImg.lastIndexOf("img")+3, selectedImg.lastIndexOf(".jpeg"));
+    if(parseInt(currentImgNumber) < 15) {
+        var nextImgSrc = "images/img" + (parseInt(currentImgNumber)+1) + ".jpeg"; 
+        lightBoxImg.setAttribute("src", nextImgSrc);
+        selectedImg = nextImgSrc;
+        if(parseInt(currentImgNumber) === 14) {
+            rarrow.classList.remove("active");
+        } 
+        if(parseInt(currentImgNumber) === 1) {
+            larrow.classList.add("active");
+        } 
+    }
+    else {
+        rarrow.classList.remove("active");
+    }
+    if(parseInt(currentImgNumber) >= 2){
+        larrow.classList.add("active");
+    }
+};
+
+var closeFn = function () {
+    lightBox.classList.remove("active");
+    close.classList.remove("active");    
+};
+
+document.querySelector(".larrow-img").addEventListener("click", leftArrowFn);
+document.querySelector(".rarrow-img").addEventListener("click", rightArrowFn);
+document.querySelector(".close-img").addEventListener("click", closeFn);
+
 
